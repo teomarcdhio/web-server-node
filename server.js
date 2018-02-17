6d23348491e3534ccf738b5ef9df3c04a3509871
teomarcdhio <mmarcolinishop@gmail.com>
2018-02-17 11:00:24 +0000

    End of Express basic

--- server.js
+++ server.js
@@ -1,23 +1,50 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view-engine', 'hbs');
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}: ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append log to file');
+    };
+  });
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance page'
+  });
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req,res ) => {
   res.render('home.hbs',{
     pageTitle: 'The home page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Hey welcome you!'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'The About page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'The About page'
   });
 });
 
