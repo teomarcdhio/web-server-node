45ed244b0bbb2e933056d40d639527480154abcb
teomarcdhio <mmarcolinishop@gmail.com>
2018-02-17 11:09:48 +0000

    Adding gitignore

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.render('maintenance.hbs', {
-    pageTitle: 'Maintenance page'
+    pageTitle: 'maintenance page'
   });
 });
 
